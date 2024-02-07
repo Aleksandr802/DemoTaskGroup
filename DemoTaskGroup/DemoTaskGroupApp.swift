041ea00e382ac6a2367a2397227d7f2ac7ed35d3
Aleksandr802 <savs802@gmail.com>
2024-02-06 16:32:50 -0800

    Initial Commit

---
+++ DemoTaskGroup/DemoTaskGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTaskGroupApp.swift
+//  DemoTaskGroup
+//
+//  Created by Oleksandr Seminov on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoTaskGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
